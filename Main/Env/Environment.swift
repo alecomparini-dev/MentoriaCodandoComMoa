dea02840f3315daa4298851ca32c0e5b70e72507
Comparini <alecomparini.dev@gmail.com>
2023-09-05 11:25:36 -0300

    include env variables and constants string

---
+++ Main/Env/Environment.swift
@@ -0,0 +1,21 @@
+//  Created by Alessandro Comparini on 05/09/23.
+//
+
+import Foundation
+
+final public class Environment {
+    
+    private static let ENV = "ENV"
+    
+    public enum Variables: String {
+        case apiBaseUrl = "API_BASE_URL"
+        case uIdFirebase = "UID_FIREBASE"
+        case defaultTheme = "DEFAULT_THEME"
+    }
+    
+    public static func variable(_ key: Environment.Variables) -> String {
+        let enviroment = Bundle.main.infoDictionary![ENV] as! [String: Any]
+        return enviroment[key.rawValue] as! String
+    }
+    
+}
---
+++ Main/Handler/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Main
+//
+//  Created by Alessandro Comparini on 05/09/23.
+//
+
+import Foundation
+
+
+struct K {
+    static let pathGetListComponent = "/Api/ThemeComponent/GetListaThemeComponent"
+    
+    struct Strings {
+        static let themeId = "themeId"
+        static let uIdFirebase = "uIdFirebase"
+    }
+}
