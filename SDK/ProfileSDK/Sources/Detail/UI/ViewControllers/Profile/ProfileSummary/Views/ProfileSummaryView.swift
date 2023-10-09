0ea1ed2dcaffbfdf0fd7c9f4954b435af7b5ce8f
Comparini <alecomparini.dev@gmail.com>
2023-10-09 11:11:04 -0300

    include profile summary on hometabviewcontroller

---
+++ SDK/ProfileSDK/Sources/Detail/UI/ViewControllers/Profile/ProfileSummary/Views/ProfileSummaryView.swift
@@ -0,0 +1,45 @@
+//  Created by Alessandro Comparini on 09/10/23.
+//
+
+import UIKit
+
+import DesignerSystemSDKComponent
+
+class ProfileSummaryView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+//  MARK: - LAZY AREA
+    lazy var backgroundView: CustomView = {
+        let comp = CustomView()
+            .setConstraints { build in
+                build
+                    .setPin.equalToSuperView
+            }
+        return comp
+    }()
+    
+    
+//  MARK: - PRIVATE AREA
+    
+    private func configure() {
+        addElements()
+        configConstraints()
+    }
+    
+    private func addElements() {
+        backgroundView.add(insideTo: self)
+    }
+    
+    private func configConstraints() {
+        backgroundView.applyConstraint()
+    }
+}
